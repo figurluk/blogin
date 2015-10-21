038b7be9baed9937ce724b8fd78c682ed2d32ae3
Lukas Figura <figurluk@gmail.com>
2015-10-21 13:56:36 +0200

    kompletna validacia v login page, pridate preklady !! v laravely

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="center-block login-block">
                 <div class="text-center m-b-md">
-                    <h2>Prihlasit sa</h2>
+                    <h2>Prihlásiť sa</h2>
 
                     @include('errors.show_error')
                 </div>
@@ -19,24 +19,38 @@
                                     <label class="control-label" for="username">Email</label>
                                     <input name="email" type="email" class="form-control" id="email"
                                            placeholder="priklad@email.sk" tabindex="1"
-                                           pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-                                           required value="{{ old('email') }}">
+                                           value="{{ old('email') }}" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                                           title="Pole musí obsahovať validnú emailovú adresu."
+                                           required>
                                     <span class="help-block small">Prihlasovacie meno je Váš email zadaný pri registrácii</span>
+                                    <span class="register-error-empty"
+                                          style="display:none">Toto pole musí byť vyplnené!</span>
+                                    <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
+                                    Pole musí obsahovať validnú emailovú adresu.</span>
                                 </div>
+
                                 <div class="form-group">
                                     <label class="control-label" for="password">Password</label>
                                     <input name="password" type="password" class="form-control" id="password"
-                                           tabindex="2" required>
+                                           tabindex="2" pattern=".{6,}" title="Pole musí obsahovať minimálne 6 znakov!"
+                                           required>
+                                    <span class="register-error-empty"
+                                          style="display:none">Toto pole musí byť vyplnené!</span>
+                                    <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
+                                        Pole musí obsahovať minimálne 6 znakov!</span>
                                 </div>
+
                                 <div class="text-center">
                                     <div class="checkbox">
-                                        <label>
+                                        <label class="control-label">
                                             <input name="remember" type="checkbox" value="remember-me" class="i-checks">
                                             Pamätaj si ma
                                         </label>
                                     </div>
                                 </div>
-                                <button class="btn btn-info btn-block" type="submit" tabindex="3">Prihlásiť sa</button>
+                                <button class="btn btn-info btn-block" type="submit" tabindex="3" id="loginSubmit">
+                                    Prihlásiť sa
+                                </button>
                                 <a class="btn btn-default btn-block"
                                    href="{{action('Auth\AuthController@getRegister')}}">Register</a>
                             </form>
@@ -51,4 +65,100 @@
             </div>
         </div>
     </div>
+@stop
+
+@section('scripts')
+    <script>
+        $(document).ready(function () {
+            $("#email").keyup(function (e) {
+                validateEmail();
+            });
+
+            $("#email").change(function (e) {
+                validateEmail();
+            });
+
+            $("#password").keyup(function (e) {
+                validatePassword("password");
+            });
+
+            $("#password").change(function (e) {
+                validatePassword("password");
+            });
+
+            $("#loginSubmit").click(function (e) {
+                if (!validatePage()) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            });
+
+            function validateEmail() {
+                var val = $('#email').val();
+                validEmail = (val.length != 0);
+                if (validEmail) {
+                    var re = /\S+@\S+\.\S+/;
+                    validEmail = re.test(val);
+
+                    if (validEmail) {
+                        validityShow($('#email'), "");
+                    } else {
+                        validityShow($('#email'), "invalid");
+                    }
+                } else {
+                    validityShow($('#email'), "empty");
+                }
+                return validEmail;
+            }
+
+            function validatePassword(inputId) {
+                var val = $('#' + inputId).val();
+                validInput = (val.length != 0);
+                if (validInput) {
+
+                    var patt = new RegExp("^[a-záäčďéěíĺľňóôöőŕřšťúüűůýžA-ZÁÄČĎĚÉÍĹĽŇÓÔÖŐŘŔŠŤÚÜŰÝŽ.,;0-9´'’()/\"\\- ]{6,}$");
+                    var res = val.search(patt);
+
+                    validInput = (res != -1);
+
+                    if (this.validInput) {
+                        validityShow($('#' + inputId), "");
+                    } else {
+                        validityShow($('#' + inputId), "invalid");
+                    }
+                } else {
+                    validityShow($('#' + inputId), "empty");
+                }
+                return validInput;
+            }
+
+            function validatePage(parent) {
+                var validEmail = validateEmail();
+                var validPass = validatePassword("password");
+
+                return (validEmail && validPass);
+            }
+
+            function validityShow(input, status) {
+                var parent = input.parent().closest('div');
+
+                if (status == "empty") {
+                    parent.addClass("has-error");
+                    parent.removeClass("has-success");
+                    parent.find('.register-error-empty').css('display', '');
+                    parent.find('.register-error-invalid').css('display', 'none');
+                } else if (status == "invalid") {
+                    parent.addClass("has-error");
+                    parent.removeClass("has-success");
+                    parent.find('.register-error-empty').css('display', 'none');
+                    parent.find('.register-error-invalid').css('display', '');
+                } else {
+                    parent.addClass("has-success");
+                    parent.removeClass("has-error");
+                    parent.find('.register-error-empty').css('display', 'none');
+                    parent.find('.register-error-invalid').css('display', 'none');
+                }
+            }
+        });
+    </script>
 @stop
