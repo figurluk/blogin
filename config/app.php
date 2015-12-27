64f0de9c4bb7115aa03ecfbd5c60ac393bc71b73
Lukáš Figura <figurluk@gmail.com>
2015-12-27 22:12:46 +0100

    prechhod na verziu laravelu 5.2

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'env' => env('APP_ENV', 'production'),
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -12,6 +13,7 @@ return [
     | application. If disabled, a simple generic error page is shown.
     |
     */
+
 
     'debug' => env('APP_DEBUG', false),
 
@@ -113,13 +115,11 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
