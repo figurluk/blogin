8649954d90d7ad3e61afccc7cd70672eec23cf5f
Lukáš Figura <figurluk@gmail.com>
2016-01-08 22:06:11 +0100

    odstranenie glyphov z proilu

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -14,8 +14,6 @@
                            value="{{ $user->name }}" tabindex="1"
                            pattern="^[a-záäčďéíĺľňóôöőŕřšťúüűýžA-ZÁÄČĎÉÍĹĽŇÓÔÖŐŘŔŠŤÚÜŰÝŽ.]+$"
                            title="Pole môže obsahovať iba písmená." required>
-                    <span class="glyphicon glyphicon-remove remove-glyph" style="display:none"></span>
-                    <span class="glyphicon glyphicon-ok ok-glyph" style="display:none"></span>
                                     <span class="register-error-empty"
                                           style="display:none">Toto pole musí byť vyplnené!</span>
                                     <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
@@ -28,8 +26,6 @@
                            value="{{ $user->surname }}" tabindex="2"
                            pattern="^[a-záäčďéíĺľňóôöőŕřšťúüűýžA-ZÁÄČĎÉÍĹĽŇÓÔÖŐŘŔŠŤÚÜŰÝŽ.]+$"
                            title="Pole môže obsahovať iba písmená." required>
-                    <span class="glyphicon glyphicon-remove remove-glyph" style="display:none"></span>
-                    <span class="glyphicon glyphicon-ok ok-glyph" style="display:none"></span>
                                     <span class="register-error-empty"
                                           style="display:none">Toto pole musí byť vyplnené!</span>
                                     <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
@@ -43,8 +39,6 @@
                            value="{{ $user->email }}" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
                            title="Pole musí obsahovať validnú emailovú adresu."
                            required>
-                    <span class="glyphicon glyphicon-remove remove-glyph" style="display:none"></span>
-                    <span class="glyphicon glyphicon-ok ok-glyph" style="display:none"></span>
 
                     <div class="message">Email je Vasim prihlasovacim menom.</div>
                                     <span class="register-error-empty"
@@ -58,8 +52,6 @@
                     <input name="password" type="password" class="form-control custom-form" value=""
                            id="password"
                            tabindex="4">
-                    <span class="glyphicon glyphicon-remove remove-glyph" style="display:none"></span>
-                    <span class="glyphicon glyphicon-ok ok-glyph" style="display:none"></span>
                                     <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
                                         Pole musí obsahovať minimálne 6 znakov!</span>
                 </div>
@@ -69,8 +61,6 @@
                     <input name="newpassword" type="password" class="form-control custom-form"
                            id="newpassword"
                            tabindex="5">
-                    <span class="glyphicon glyphicon-remove remove-glyph" style="display:none"></span>
-                    <span class="glyphicon glyphicon-ok ok-glyph" style="display:none"></span>
                                     <span class="register-error-empty"
                                           style="display:none">Toto pole musí byť vyplnené!</span>
                                     <span class="register-error-invalid" style="display:none">Toto pole nie je vyplnené správne!
@@ -224,23 +214,17 @@
                     parent.addClass("has-error");
                     parent.removeClass("has-success");
                     parent.find('.register-error-empty').css('display', '');
-                    parent.find('.remove-glyph').css('display', '');
-                    parent.find('.ok-glyph').css('display', 'none');
                     parent.find('.register-error-invalid').css('display', 'none');
                 } else if (status == "invalid") {
                     parent.addClass("has-error");
                     parent.removeClass("has-success");
                     parent.find('.register-error-empty').css('display', 'none');
                     parent.find('.register-error-invalid').css('display', '');
-                    parent.find('.remove-glyph').css('display', '');
-                    parent.find('.ok-glyph').css('display', 'none');
                 } else {
                     parent.addClass("has-success");
                     parent.removeClass("has-error");
                     parent.find('.register-error-empty').css('display', 'none');
                     parent.find('.register-error-invalid').css('display', 'none');
-                    parent.find('.remove-glyph').css('display', 'none');
-                    parent.find('.ok-glyph').css('display', '');
                 }
             }
 
